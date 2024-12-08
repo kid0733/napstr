ec638710d3b97c5d8719b847aaae4153bca3bae2
Siddhant Singh Karki <siddhantskarki@gmail.com>
2024-12-08 23:01:50 +1100

    Initial commit: Setup React Native project with Expo

---
+++ src/app/_layout.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function App() {
+	return (
+		<View style={styles.container}>
+			<Text style={{ color: '#ffffff' }}>Open up App.tsx to start working on your app!</Text>
+			<StatusBar style="auto" />
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#000000',
+		alignItems: 'center',
+		justifyContent: 'center',
+		color: '#ffffff',
+	},
+	
+})
